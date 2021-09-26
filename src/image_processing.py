8544b2a8a2b2b20033092e5f0168bb09a736a041
Eric H. Lewis <64712530+ericlewisX@users.noreply.github.com>
2021-09-26 00:20:57 -0400

    Add files via upload

--- src/image_processing.py
+++ src/image_processing.py
@@ -11,20 +11,19 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 
 
 # Functions
-
 def grayscale(img):
     '''
-    This function takes in a feature matrix X (scales it down using MinMaxScaler()), and a target array y, and builds a Random Forest model using them.
+    Takes in an image and returns its grayscaled transformation.
     
     Parameters
     ----------
-    img : pandas Array or pandas Dataframe
-        [Feature matrix]
+    img : array
+        [The image file.]
         
     Returns
     -------
-    img_expanded : sklearn.tree._classes.RandomForestClassifier
-        [Fitted Random Forest Model built with a 0.80 training size, and max_iter 1000.]
+    img_expanded : array
+        [The grayscaled image.]
     '''
     img = img.astype(np.float32)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -33,17 +32,17 @@ def grayscale(img):
 
 def equalize(img):
     '''
-    This function takes in a feature matrix X (scales it down using MinMaxScaler()), and a target array y, and builds a Random Forest model using them.
+    This function takes in an image and redistributes the brightness values of the pixels in an image so the image more evenly represents the entire range of brightness levels.
     
     Parameters
     ----------
-    img : pandas Array or pandas Dataframe
-        [Feature matrix]
-
+    img : array
+        [The image file.]
+        
     Returns
     -------
-    img : sklearn.tree._classes.RandomForestClassifier
-        [Fitted Random Forest Model built with a 0.80 training size, and max_iter 1000.]
+    img : array
+        [The equalized image.]
     '''
     img1 = np.uint8(cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX))
     img = cv2.equalizeHist(img1)
@@ -51,43 +50,43 @@ def equalize(img):
 
 def preprocessing(img):
     '''
-    This function takes in a feature matrix X (scales it down using MinMaxScaler()), and a target array y, and builds a Random Forest model using them.
+    This function takes in an image and returns an image that is greyscaled, equalized and standardized.
     
     Parameters
     ----------
-    img : pandas Array or pandas Dataframe
-        [Feature matrix]
-
+    img : array
+        [The image file.]
+        
     Returns
     -------
-    img : sklearn.tree._classes.RandomForestClassifier
-        [Fitted Random Forest Model built with a 0.80 training size, and max_iter 1000.]
+    img : array
+        [The fully preprocessed image.]
     '''
     img = grayscale(img)     
-    img = equalize(img)      # STANDARDIZE THE LIGHTING IN AN IMAGE
+    img = equalize(img)     
     img = img / 255    
     return img
 
 def depth_adder(X_train, X_validation, X_test):
     '''
-    This function takes in a feature matrix X (scales it down using MinMaxScaler()), and a target array y, and builds a Random Forest model using them.
+    This function takes in the X_train, X_validation, & X_test sets and adds another dimension to each image.
     
     Parameters
     ----------
-    X_train : pandas Array or pandas Dataframe
+    X_train : array
         [Feature matrix]
-    X_validation : pandas Series
+    X_validation : arra
         [Target array]
-    X_test : kwargs for `RandomForestClassifier()`
+    X_test : array
     
     Returns
     -------
-    X_train : sklearn.tree._classes.RandomForestClassifier
-        [Fitted Random Forest Model built with a 0.80 training size, and max_iter 1000.]
-    X_validation : numpy array
-        [X_test made from train_test_split]
+    X_train : image array
+        [An array of all the images in the training set now with one extra dimension.]
+    X_validation : image array
+        [An array of all the images in the validation set now with one extra dimension.]
     X_test : pandas Series
-        [y_test made from train_test_split]
+        [An array of all the images in the test set now with one extra dimension.]
     '''
     X_train = X_train.reshape(X_train.shape[0], 
                             X_train.shape[1], 
